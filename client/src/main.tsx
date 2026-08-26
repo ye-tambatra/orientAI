c48cdb3bf25c97fa93028b36f1fcf5934b1b3295
Onja1234 <onjafanevarinoh@gmail.com>
2026-08-26 17:34:49 +0300

    feat: add material ui

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import CssBaseline from '@mui/material/CssBaseline'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>,
 )
